9dd9c22c3bcae2417791219c54572295ca8b0485
robbykansas <lomiver@gmail.com>
2021-01-14 20:15:58 +0700

    get orchestrator

--- server/orchestrator/controllers/orchestratorController.js
+++ server/orchestrator/controllers/orchestratorController.js
@@ -34,6 +34,26 @@ class orchestratorController {
       res.send(e)
     }
   }
+  
+  static async getOrchestrator(req, res){
+    try{
+      const cache = await redis.get('orchestrator')
+      if (cache) {
+        res.send(JSON.parse(cache))
+      } else {
+        const tv = await axios.get('http://localhost:4002/tv')
+        const movies = await axios.get('http://localhost:4001/movies')
+        const data = {
+          movies: movies.data,
+          tvSeries: tv.data
+        }
+        await redis.set('orchestrator', JSON.stringify(data))
+        res.send(data)
+      }
+    } catch (e) {
+      res.send(e)
+    }
+  }
 }
 
 module.exports = orchestratorController
